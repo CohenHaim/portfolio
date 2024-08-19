04e515373acec6422d7e6bb1edb537de8721126c
CohenHaim <haim.nova@gmail.com>
2024-08-19 08:59:35 +0300

    added configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,35 @@
-import { builder } from "@builder.io/sdk";
-import type { BuilderContent } from "@builder.io/sdk";
-import { RenderBuilderContent } from "../components/builder";
-import { env } from "~/env";
-
-builder.init(env.NEXT_PUBLIC_BUILDERIO_API_KEY);
+import { Content, fetchOneEntry, isEditing, isPreviewing } from '@builder.io/sdk-react';
+import { env } from '~/env';
 
 interface PageProps {
-  params: {
-    page: string[];
-  };
+  params: { slug: string[] };
+  searchParams: Record<string, string>;
 }
 
 export default async function HomePage(props: PageProps) {
-  const model = "page";
-  const content: BuilderContent = await builder.get("page", {
-      userAttributes: {
-        urlPath: "/" + (props?.params?.page?.join("/") ?? ""),
-      },
-      prerender: false,
-    })
-    .toPromise() as BuilderContent;
+  // NOTE: the import must be inside the Page component itself.
+  const { initializeNodeRuntime } = await import('@builder.io/sdk-react/node/init');
+  initializeNodeRuntime();
 
-  return (
-    <>
-      <RenderBuilderContent content={content} model={model} />
-    </>
-  );
+  const urlPath = '/' + (props.params?.slug?.join('/') || '');
+
+  const content = await fetchOneEntry({
+    options: props.searchParams,
+    apiKey: env.NEXT_PUBLIC_BUILDERIO_API_KEY,
+    model: 'page',
+    userAttributes: { urlPath },
+  });
+
+  const canShowContent =
+    content || isPreviewing(props.searchParams) || isEditing(props.searchParams);
+
+  if (!canShowContent) {
+    return (
+      <>
+        <h1>404</h1>
+        <p>Make sure you have your content published at builder.io.</p>
+      </>
+    );
+  }
+  return <Content content={content} apiKey={env.NEXT_PUBLIC_BUILDERIO_API_KEY} model={'page'} />;
 }
