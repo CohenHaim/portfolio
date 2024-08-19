771c9f7bcaa1d84973e94987307bb8c061f1e794
CohenHaim <haim.nova@gmail.com>
2024-08-19 09:08:46 +0300

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Haim Cohen Portfolio",
@@ -14,6 +15,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
+      <Analytics/>
       <body>{children}</body>
     </html>
   );
