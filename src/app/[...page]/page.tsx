409306f8f8faef11762e3093ea5be6dfcc0e9a0b
CohenHaim <haim.nova@gmail.com>
2024-08-19 08:40:15 +0300

    added assert to BuilderContent

--- src/app/[...page]/page.tsx
+++ src/app/[...page]/page.tsx
@@ -1,8 +1,8 @@
 import { builder } from "@builder.io/sdk";
+import type { BuilderContent } from "@builder.io/sdk";
 import { RenderBuilderContent } from "../../components/builder";
 import { env } from "~/env";
 
-// Replace with your Public API Key
 builder.init(env.NEXT_PUBLIC_BUILDERIO_API_KEY);
 
 interface PageProps {
@@ -13,22 +13,16 @@ interface PageProps {
 
 export default async function Page(props: PageProps) {
   const model = "page";
-  const content = await builder
-    // Get the page content from Builder with the specified options
-    .get("page", {
+  const content: BuilderContent = await builder.get("page", {
       userAttributes: {
-        // Use the page path specified in the URL to fetch the content
-        urlPath: "/" + (props?.params?.page?.join("/") || ""),
+        urlPath: "/" + (props?.params?.page?.join("/") ?? ""),
       },
-      // Set prerender to false to return JSON instead of HTML
       prerender: false,
     })
-    // Convert the result to a promise
-    .toPromise();
+    .toPromise() as BuilderContent;
 
   return (
     <>
-      {/* Render the Builder page */}
       <RenderBuilderContent content={content} model={model} />
     </>
   );
--- src/components/builder.tsx
+++ src/components/builder.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { ComponentProps } from "react";
+import type { ComponentProps } from "react";
 import { BuilderComponent, useIsPreviewing } from "@builder.io/react";
-import { BuilderContent, builder } from "@builder.io/sdk";
+import { builder } from "@builder.io/sdk";
 import DefaultErrorPage from "next/error";
 import { env } from "~/env";
 
@@ -16,7 +16,7 @@ export function RenderBuilderContent(props: BuilderPageProps) {
   const isPreviewing = useIsPreviewing();
   // If `content` has a value or the page is being previewed in Builder,
   // render the BuilderComponent with the specified content and model props.
-  if (props.content || isPreviewing) {
+  if (props.content ?? isPreviewing) {
     return <BuilderComponent {...props} />;
   }
   // If the `content` is falsy and the page is
